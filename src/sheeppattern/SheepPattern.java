fbd4c579bb6a8bb8c2e76c81f6be334f590e591a
Ernestinebot <116901955+Ernestinebot@users.noreply.github.com>
2022-12-02 11:21:18 -0500

    Initial commit

---
+++ src/sheeppattern/SheepPattern.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sheeppattern;
+
+/**
+ *
+ * @author ernestine More practice with sout tab and System.out.println
+ */
+public class SheepPattern {   //must be saved as SheepPattern.java
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("            '  '");
+         System.out.println("             ___");
+         System.out.println("            (oo)");
+         System.out.println("  /==========//");
+         System.out.println(" /  || @@ ||");
+         System.out.println("*   ||----||");
+         System.out.println("    VV    VV");
+         System.out.println("    ..    ..");
+    }
+    
+}
